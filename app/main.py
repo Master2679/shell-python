85298eb96cb0e7927fd82e498795d3570f4a8eea
root <root@LAPTOP-OA4L7FUQ>
2024-06-01 20:37:05 -0700

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -4,12 +4,19 @@ def main():
     This function prompts the user for input and waits for user input.
     """
 
-    sys.stdout.write("$ ")
-    sys.stdout.flush()
+
 
     # Wait for user input
-    command = input()
-    print(command + ": command not found\n")
+    valid_commands = []
+    while True:
+        sys.stdout.write("$ ")
+        sys.stdout.flush()
+        user_command = input()
+        if user_command not in valid_commands:
+            print(f"{user_command}: command not found")
+            continue
+
+
 
 if __name__ == "__main__":
     main()
